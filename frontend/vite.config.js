4f18adc7163a6fe8ce57faf952ce318836ee5c9d
bhanu prakash <bhanuprakashkuruva09@gmail.com>
2024-12-20 21:50:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // This will proxy any request starting with /customer to the backend URL
+      '/customer': {
+        target: 'https://restaurant-app-three-pied.vercel.app', // Backend URL
+        changeOrigin: true, // Ensures the origin of the request matches the target
+        // secure: false, // Set to false if the backend is HTTP (and not HTTPS)
+      },
+    },
+  },
+});
+
